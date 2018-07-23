cf6e862c0fc6c537ff926c5736cf43b838e119a0
pgrcommitter <info@pgrfirm.com>
2018-07-23 17:12:39 -0400

    more

--- Vagrantfile
+++ Vagrantfile
@@ -1,20 +1,20 @@
-NODE_COUNT = 2
+NODE_COUNT = 1
 
 
 Vagrant.configure("2") do |config|  
+    
   
-  config.vm.box = "centos/7"
-  config.vm.provision "shell", path: "bootstrap-common.sh"
-  
-  config.vm.define "master", primary: true do |subconfig|
-	subconfig.vm.hostname = "master"
-	subconfig.vm.network "private_network", ip: "10.0.0.10"
-	subconfig.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
-	subconfig.vm.provision "shell", path: "bootstrap-master.sh"
+  config.vm.define "master", primary: true do |master|
+    master.vm.box = "centos/7"
+	master.vm.hostname = "master"
+	master.vm.network "private_network", ip: "10.0.0.10"	
+	master.vm.provision "shell", path: "bootstrap-master.sh"
   end
+  
   
   (1..NODE_COUNT).each do |i|
     config.vm.define "node#{i}" do |subconfig|
+	  subconfig.vm.box = "centos/7"
       subconfig.vm.hostname = "node#{i}"
 	  subconfig.vm.network "private_network", ip: "10.0.0.#{i+10}"
     end
--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -1,4 +1,30 @@
+sudo yum -y install epel-release
+sudo yum -y install nss-mdns
+sudo yum -y install wget
+
+#nginx
 sudo yum -y install nginx
 sudo systemctl start nginx
-sudo systemctl status nginx
 sudo systemctl enable nginx
+
+#ntp
+sudo yum -y install ntp
+sudo timedatectl set-timezone America/New_York
+sudo systemctl start ntpd
+sudo systemctl status ntpd
+sudo systemctl enable ntpd
+
+
+#java
+echo "Downloading Oracle Java ..."
+wget --no-verbose --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.rpm"
+sudo yum -y localinstall jdk-8u181-linux-x64.rpm
+rm -rf jdk-8u181-linux-x64.rpm
+
+
+#oracle 
+sudo groupadd -g 501 oinstall
+sudo groupadd -g 502 dba
+sudo groupadd -g 503 oper
+sudo useradd -u 500 -g oinstall -G dba,oper oracle
+sudo usermod -g oinstall -G dba,oper oracle
